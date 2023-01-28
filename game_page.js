12fac1afdcb235c4bd38e91c53c6ecf77e7607d4
AarnaBist <114805579+AarnaBist@users.noreply.github.com>
2023-01-28 17:04:39 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -70,17 +70,14 @@ function check()
      document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ; 
     }
 
-
-    if(answer_turn == "player1")
-    {
-        answer_turn = "player2";
-        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
-    }
-    else
-    {
-        answer_turn = "player1"
-        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
-    }
+if(answer_turn == "player1")
+{ answer_turn = "player2" ;
+ document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+} 
+    else 
+{ answer_turn = "player1";
+ document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ; 
+}
 
     document.getElementById("output").innerHTML = "";
 }
